6ffabfec41f994d3b342232f557bbcbb84ffaea3
Rithvika Reddy <rithvikareddy82@gmail.com>
2022-11-23 12:14:53 +0530

    internal2

---
+++ src/main/java/com/demo/maven/javalab/App.java
@@ -0,0 +1,13 @@
+package com.demo.maven.javalab;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "thsi is se labinternal 1 exam,the created maven project is ExamWeb" );
+    }
+}
